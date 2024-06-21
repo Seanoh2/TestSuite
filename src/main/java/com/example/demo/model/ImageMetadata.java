f874311d1e752148f3b33b483317894fa23c18ca
Sean O'Hora <seanoh2@live.com>
2024-06-21 16:34:24 +0100

    Implemented navbar, Updated validation options

---
+++ src/main/java/com/example/demo/model/ImageMetadata.java
@@ -0,0 +1,81 @@
+package com.example.demo.model;
+
+import java.util.Objects;
+
+public class PNGMetadata {
+    private String name;
+    private int width;
+    private int height;
+    private boolean matching;
+    private boolean pngFormated;
+
+    public PNGMetadata() {
+    }
+
+    public PNGMetadata(String name, int width, int height) {
+        this.name = name;
+        this.width = width;
+        this.height = height;
+    }
+
+    public PNGMetadata(String name, int width, int height, boolean matching, boolean pngFormated) {
+        this.name = name;
+        this.width = width;
+        this.height = height;
+        this.matching = matching;
+        this.pngFormated = pngFormated;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public boolean isMatching() {
+        return matching;
+    }
+
+    public void setMatching(boolean matching) {
+        this.matching = matching;
+    }
+
+    public boolean isPngFormated() {
+        return pngFormated;
+    }
+
+    public void setPngFormated(boolean pngFormated) {
+        this.pngFormated = pngFormated;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PNGMetadata that = (PNGMetadata) o;
+        return width == that.width && height == that.height && matching == that.matching && pngFormated == that.pngFormated && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, width, height, matching, pngFormated);
+    }
+}
